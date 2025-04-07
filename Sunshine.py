53253f8440e5089919bb8c625251ecf2615ded8b
evanschmelkin <evanschmelkin@gmail.com>
2025-04-06 22:41:41 -0400

    sunshine map first

---
+++ Sunshine.py
@@ -0,0 +1,28 @@
+from geopy import geocoders #this will turn city names into geographic points
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+import pandas as pd
+wikiurl = 'https://en.wikipedia.org/wiki/List_of_cities_by_sunshine_duration'
+tables = pd.read_html(wikiurl)
+
+
+
+#make tables into variables (except europe)
+africa = tables[0]
+asia = tables[1]
+northamerica = tables[3]
+southamerica = tables[4]
+oceania = tables[5]
+
+
+#europe is a special case
+europeurl = 'https://en.wikipedia.org/wiki/List_of_cities_in_Europe_by_sunshine_duration'
+europe = pd.read_html(europeurl)
+
+# for table in tables:
+
+northamericayears =
+
+print(df)
